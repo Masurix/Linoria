6368facdfba7411078581083034209291b91ba4d
Masurix <110713912+Masurix@users.noreply.github.com>
2023-02-10 20:03:43 +0700

    Update uimain.lua

--- uimain.lua
+++ uimain.lua
@@ -1,3 +1,5 @@
+RainbowStep = true
+
 local InputService = game:GetService('UserInputService');
 local TextService = game:GetService('TextService');
 local TweenService = game:GetService('TweenService');
@@ -29,10 +31,10 @@ local Library = {
     HudRegistry = {};
 
     FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(0, 15, 30);
-    BackgroundColor = Color3.fromRGB(5, 5, 20);
-    AccentColor = Color3.fromRGB(0, 180, 240);
-    OutlineColor = Color3.fromRGB(0, 0, 5);
+    MainColor = Color3.fromRGB(24, 24, 37);
+    BackgroundColor = Color3.fromRGB(22, 22, 31);
+    AccentColor = Color3.fromRGB(0, 255, 255);
+    OutlineColor = Color3.fromRGB(50, 50, 50);
     
     Black = Color3.new(0, 0, 0);
 
